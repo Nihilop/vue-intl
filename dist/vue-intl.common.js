b971aa8c655cceb288032a48d635296f289328bb
Tobbe <contact@tobbexiv.de>
2020-03-15 13:54:36 +0100

    Fix issue with usage of this in filters.

--- dist/vue-intl.common.js
+++ dist/vue-intl.common.js
@@ -709,10 +709,10 @@ function plugin(Vue) {
     vue.$currency = formatCurrency;
     vue.$relativeDate = relativeDate;
 
-    Vue.filter('date', formatDate);
-    Vue.filter('number', formatNumber);
-    Vue.filter('currency', formatCurrency);
-    Vue.filter('relativeDate', relativeDate);
+    Vue.filter('date', function(date, format, timezone) { vue.$date(date, format, timezone); });
+    Vue.filter('number', function(number, fractionSize) { vue.$number(number, fractionSize); });
+    Vue.filter('currency', function(amount, currencySymbol, fractionSize) { vue.$currency(amount, currencySymbol, fractionSize); });
+    Vue.filter('relativeDate', function(date, options) { vue.$relativeDate(date, options); });
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
